1a12f80a5182123eadaf4cc8168a529fe70b6b0a
AyanDesai-code <ayan.k.desai@gmail.com>
2024-12-23 12:48:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+def reverse_string(thing):
+    is_palindrome=False
+    reversed_thing=thing[::-1]
+    if reversed_thing==thing:
+        print("True")
+    else:
+        print("False")
+
+
+
+
+
+reverse_string("bob")
